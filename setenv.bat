979d08c1ebddb5c30c56eb28e1d71c8bc1aaba9a
Peter Molchanov <pmolchanov@users.noreply.github.com>
2015-03-23 23:36:22 -0700

    Update setenv.bat

--- setenv.bat
+++ setenv.bat
@@ -1,10 +1,13 @@
 @echo off
 
-REM Root directory of the Windows 7 DDK
-set DDK_HOME=C:\WINDDK\7600.16385.1
-
 REM Password used for PFX file
 set PFX_PWD=TODO
+
+REM Set up a 64-bit Windows Server 2003 checked build environment
+set BUILD_ENV=chk WNET x64
+
+REM Root directory of the Windows 7 DDK
+set DDK_HOME=C:\WINDDK\7600.16385.1
 
 REM Name used for binaries and service
 set TARGETNAME=HelloWorld
@@ -17,5 +20,5 @@ set MSC_WARNING_LEVEL=/W3 /WX
 REM Configure DDK build environment
 REM setenv.bat Directory [fre|chk] [x86|64|x64] [hal] [WIN7|WLH|WXP|WNET] [no_prefast] [bscmake] [separate_object_root][oacr|no_oacr]
 pushd %HELLOWORLD_HOME%
-call %DDK_HOME%\bin\setenv.bat %DDK_HOME% chk WNET x64
+call %DDK_HOME%\bin\setenv.bat %DDK_HOME% %BUILD_ENV%
 popd
